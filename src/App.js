867f2a4ce266cbb973c4e63c1ec0e6ae0d25ef3e
kushalshinde0 <kushalshinde19@gmail.com>
2024-11-15 12:00:53 +0530

    Case_study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import DoctorsTable  from "./DoctorsTable";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+
+  const [userName, setUserName] = useState();
+  //Provide the background image details..
+  const myStyle = {
+    backgroundImage: 'url(wal9.png)',
+    
+    backgroundSize: 'cover',
+    height: '100%'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={myStyle}>
+<DoctorsTable/>
+    </ div>
   );
 }
-
 export default App;
